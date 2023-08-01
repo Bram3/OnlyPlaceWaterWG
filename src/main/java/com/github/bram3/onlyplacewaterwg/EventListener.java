cf8afd8dfe0b61be0f52aa859bf5e2516458274b
Bram Verpoot <bramverpoot@gmail.com>
2023-08-01 16:15:45 +0200

    Members can bypass the only water flag

--- src/main/java/com/github/bram3/onlyplacewaterwg/EventListener.java
+++ src/main/java/com/github/bram3/onlyplacewaterwg/EventListener.java
@@ -1,9 +1,11 @@
 package com.github.bram3.onlyplacewaterwg;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import com.sk89q.worldguard.session.SessionManager;
 import org.bukkit.Location;
@@ -31,9 +33,8 @@ public class EventListener implements Listener {
         if (event.getClickedBlock() == null) return;
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if (event.getItem() == null) return;
-        if (event.getItem().getType() == Material.WATER_BUCKET) return;
+        if (event.getItem().getType() == Material.WATER_BUCKET || event.getItem().getType() == Material.BUCKET) return;
         Location location = event.getPlayer().getLocation();
-
         RegionQuery query = WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery();
         com.sk89q.worldedit.util.Location wrappedLocation = BukkitAdapter.adapt(location);
         ApplicableRegionSet set = query.getApplicableRegions(wrappedLocation);
@@ -41,14 +42,22 @@ public class EventListener implements Listener {
         Player player = event.getPlayer();
         World world = player.getWorld();
         if (hasBypass(player, world)) return;
+        try {
+            ApplicableRegionSet regionSet = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world)).getApplicableRegions(BlockVector3.at(location.getX(), location.getY(), location.getZ()));
+            for (ProtectedRegion region : regionSet.getRegions()) {
+                if (region.isMember(plugin.worldGuardPlugin.wrapPlayer(player))) return;
+            }
+        } catch (NullPointerException e) {
+            // No regions found, continue
+        }
         event.setCancelled(true);
-        player.sendMessage(config.getColoredMessage("error_messages.cant_place_block"));
+        player.sendMessage(config.getColoredMessage("error_messages.cant_interact_with_block"));
 
     }
+
     private boolean hasBypass(Player player, World world) {
         com.sk89q.worldedit.world.World wrappedWorld = BukkitAdapter.adapt(world);
         LocalPlayer wrappedPlayer = plugin.worldGuardPlugin.wrapPlayer(player);
-
         SessionManager sessionManager = WorldGuard.getInstance().getPlatform().getSessionManager();
         return sessionManager.hasBypass(wrappedPlayer, wrappedWorld);
     }
